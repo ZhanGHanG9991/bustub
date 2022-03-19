17968736696bd84a05ed015deae937fd1fe1e5dd
ZhanGHanG9991 <zhanghang21@m.fudan.edu.cn>
2022-03-19 05:43:24 +0000

    P3 delete

--- src/execution/delete_executor.cpp
+++ src/execution/delete_executor.cpp
@@ -18,10 +18,25 @@ namespace bustub {
 
 DeleteExecutor::DeleteExecutor(ExecutorContext *exec_ctx, const DeletePlanNode *plan,
                                std::unique_ptr<AbstractExecutor> &&child_executor)
-    : AbstractExecutor(exec_ctx) {}
+    : AbstractExecutor(exec_ctx), plan_(plan), child_executor_(std::move(child_executor)) {
+  table_info_ = exec_ctx->GetCatalog()->GetTable(plan_->TableOid());
+  index_infos_ = exec_ctx->GetCatalog()->GetTableIndexes(table_info_->name_);
+}
 
-void DeleteExecutor::Init() {}
+void DeleteExecutor::Init() { child_executor_->Init(); }
 
-bool DeleteExecutor::Next([[maybe_unused]] Tuple *tuple, RID *rid) { return false; }
+bool DeleteExecutor::Next([[maybe_unused]] Tuple *tuple, RID *rid) {
+  if (child_executor_->Next(tuple, rid)) {
+    table_info_->table_->MarkDelete(*rid, exec_ctx_->GetTransaction());
+    for (auto index_info : index_infos_) {
+      auto key_tuple =
+          tuple->KeyFromTuple(table_info_->schema_, index_info->key_schema_, index_info->index_->GetKeyAttrs());
+      index_info->index_->DeleteEntry(key_tuple, *rid, exec_ctx_->GetTransaction());
+    }
+    return true;
+  } else {
+    return false;
+  }
+}
 
 }  // namespace bustub
