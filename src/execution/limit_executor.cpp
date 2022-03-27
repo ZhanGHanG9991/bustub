37064ffab939385d4e0bcb932733fb09d1dc3835
ZhanGHanG9991 <zhanghang21@m.fudan.edu.cn>
2022-03-27 14:34:56 +0000

    P3 limit

--- src/execution/limit_executor.cpp
+++ src/execution/limit_executor.cpp
@@ -16,10 +16,18 @@ namespace bustub {
 
 LimitExecutor::LimitExecutor(ExecutorContext *exec_ctx, const LimitPlanNode *plan,
                              std::unique_ptr<AbstractExecutor> &&child_executor)
-    : AbstractExecutor(exec_ctx) {}
+    : AbstractExecutor(exec_ctx), plan_(plan), child_executor_(std::move(child_executor)) {}
 
-void LimitExecutor::Init() {}
+void LimitExecutor::Init() {
+  child_executor_->Init();
+  cnt_ = 0;
+}
 
-bool LimitExecutor::Next(Tuple *tuple, RID *rid) { return false; }
+bool LimitExecutor::Next(Tuple *tuple, RID *rid) {
+  if (child_executor_->Next(tuple, rid) && cnt_++ < plan_->GetLimit()) {
+    return true;
+  }
+  return false;
+}
 
 }  // namespace bustub
