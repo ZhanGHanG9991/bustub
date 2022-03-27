ee6b99b8f4c63982ba14fe1406ef1aac12ce444d
ZhanGHanG9991 <zhanghang21@m.fudan.edu.cn>
2022-03-27 14:25:45 +0000

    P3 aggregation

--- src/execution/aggregation_executor.cpp
+++ src/execution/aggregation_executor.cpp
@@ -13,16 +13,52 @@
 #include <vector>
 
 #include "execution/executors/aggregation_executor.h"
+#include "execution/expressions/aggregate_value_expression.h"
 
 namespace bustub {
 
 AggregationExecutor::AggregationExecutor(ExecutorContext *exec_ctx, const AggregationPlanNode *plan,
                                          std::unique_ptr<AbstractExecutor> &&child)
-    : AbstractExecutor(exec_ctx) {}
+    : AbstractExecutor(exec_ctx),
+      plan_(plan),
+      child_(std::move(child)),
+      aht_(plan_->GetAggregates(), plan_->GetAggregateTypes()),
+      aht_iterator_(aht_.Begin()) {
+  child_->Init();
+  Tuple tuple;
+  RID rid;
+  while (child_->Next(&tuple, &rid)) {
+    aht_.InsertCombine(MakeAggregateKey(&tuple), MakeAggregateValue(&tuple));
+  }
+}
 
-void AggregationExecutor::Init() {}
+void AggregationExecutor::Init() {
+  child_->Init();
+  aht_iterator_ = aht_.Begin();
+}
 
-bool AggregationExecutor::Next(Tuple *tuple, RID *rid) { return false; }
+bool AggregationExecutor::Next(Tuple *tuple, RID *rid) {
+  while (aht_iterator_ != aht_.End()) {
+    auto temp_iterator = aht_iterator_;
+    ++aht_iterator_;
+    if (plan_->GetHaving() != nullptr) {
+      auto value =
+          plan_->GetHaving()->EvaluateAggregate(temp_iterator.Key().group_bys_, temp_iterator.Val().aggregates_);
+      if (!value.GetAs<bool>()) {
+        continue;
+      }
+    }
+    std::vector<Value> value;
+    value.reserve(plan_->OutputSchema()->GetColumnCount());
+    for (const auto &column : plan_->OutputSchema()->GetColumns()) {
+      auto agg_expr = reinterpret_cast<const AggregateValueExpression *>(column.GetExpr());
+      value.push_back(agg_expr->EvaluateAggregate(temp_iterator.Key().group_bys_, temp_iterator.Val().aggregates_));
+    }
+    *tuple = Tuple(value, plan_->OutputSchema());
+    return true;
+  }
+  return false;
+}
 
 const AbstractExecutor *AggregationExecutor::GetChildExecutor() const { return child_.get(); }
 
