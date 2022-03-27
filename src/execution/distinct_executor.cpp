d3d793f018aadacda3f32334f8e4fa854b516108
ZhanGHanG9991 <zhanghang21@m.fudan.edu.cn>
2022-03-27 15:27:00 +0000

    P3 distinct

--- src/execution/distinct_executor.cpp
+++ src/execution/distinct_executor.cpp
@@ -16,10 +16,22 @@ namespace bustub {
 
 DistinctExecutor::DistinctExecutor(ExecutorContext *exec_ctx, const DistinctPlanNode *plan,
                                    std::unique_ptr<AbstractExecutor> &&child_executor)
-    : AbstractExecutor(exec_ctx) {}
+    : AbstractExecutor(exec_ctx), plan_(plan), child_executor_(std::move(child_executor)) {}
 
-void DistinctExecutor::Init() {}
+void DistinctExecutor::Init() { child_executor_->Init(); }
 
-bool DistinctExecutor::Next(Tuple *tuple, RID *rid) { return false; }
+bool DistinctExecutor::Next(Tuple *tuple, RID *rid) {
+  while (child_executor_->Next(tuple, rid)) {
+    DistinctKey key;
+    for (uint32_t i = 0; i < plan_->GetChildPlan()->OutputSchema()->GetColumnCount(); i++) {
+      key.group_bys_.push_back(tuple->GetValue(plan_->OutputSchema(), i));
+    }
+    if (hash_table_.count(key) == 0) {
+      hash_table_.insert({key, 1});
+      return true;
+    }
+  }
+  return false;
+}
 
 }  // namespace bustub
